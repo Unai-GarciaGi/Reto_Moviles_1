01f5bf86d6a0aae4b0d75470e52e747e7ba6fd4e
in2dam <in2dam@elorrieta.int>
2021-10-26 12:45:13 +0200

    El boton comprueba la contraseña

--- app/src/main/java/com/example/reto1/MainActivity.java
+++ app/src/main/java/com/example/reto1/MainActivity.java
@@ -2,13 +2,35 @@ package com.example.reto1;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Context;
+import android.content.SharedPreferences;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.EditText;
+import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
+
+    private EditText editNombre;
+    private EditText editContrasena;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        editNombre = findViewById(R.id.editNombre);
+        editContrasena = findViewById(R.id.editContrasena);
+
+    }
+
+    public void clickBotonEntrar(View view){
+        SharedPreferences preferences = getSharedPreferences("contrasena", Context.MODE_PRIVATE);
+        String contrasenaPreferences = preferences.getString("contrasena", "");
+        if(editContrasena.getText().toString().equals(contrasenaPreferences)) {
+            //Aqui entra a la siguiente pantalla
+        }else{
+            Toast.makeText(this,R.string.errorContrasena,Toast.LENGTH_LONG).show();
+        }
     }
 }
