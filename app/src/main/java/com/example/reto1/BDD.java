edfde5fd37b75dc9d3190b55463c50a0523c1959
in2dam <in2dam@elorrieta.int>
2021-10-27 08:28:13 +0200

    Botones pantalla principal. Clase de SQLite.

---
+++ app/src/main/java/com/example/reto1/BDD.java
@@ -0,0 +1,27 @@
+package com.example.reto1;
+
+import android.content.Context;
+import android.database.sqlite.SQLiteDatabase;
+import android.database.sqlite.SQLiteOpenHelper;
+
+import androidx.annotation.Nullable;
+
+public class BDD extends SQLiteOpenHelper {
+
+    public BDD(@Nullable Context context, @Nullable String name, @Nullable SQLiteDatabase.CursorFactory factory, int version) {
+        super(context, name, factory, version);
+    }
+
+    @Override
+    public void onCreate(SQLiteDatabase db) {
+        db.execSQL("CREATE TABLE tarea(codigo int primary key, nombre text, fecha date, coste int, prioridad text, realizada boolean)");
+    }
+
+    @Override
+    public void onUpgrade(SQLiteDatabase sqLiteDatabase, int i, int i1) {
+
+    }
+
+    //Nombre, Descripción, fecha, coste, prioridad (urgente, alta, media, baja), si la
+    //tareas está hecha o no.
+}
