08c6e063c1a342ed8a0dd09850bdfef6b9f8d8bc
asierhernandezga <asier.hernandezga@elorrieta-errekamari.com>
2021-11-02 12:57:15 +0100

    Cambios en la pantallaNuevaTarea añadidos

--- app/src/main/java/com/example/reto1/PantallaNuevaTarea.java
+++ app/src/main/java/com/example/reto1/PantallaNuevaTarea.java
@@ -3,16 +3,33 @@ package com.example.reto1;
 import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Context;
+import android.content.SharedPreferences;
 import android.os.Bundle;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.view.View;
+import android.widget.ArrayAdapter;
+import android.widget.EditText;
+import android.widget.Spinner;
+import android.widget.Toast;
 
 public class PantallaNuevaTarea extends AppCompatActivity {
+
+    private EditText editTextNombre;
+    private EditText editTextDescripcion;
+    private EditText editTextFecha;
+    private EditText editTextCoste;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_pantalla_nueva_tarea);
+
+        editTextNombre = findViewById(R.id.editTextNombre);
+        editTextDescripcion = findViewById(R.id.editTextDescripcion);
+        editTextFecha = findViewById(R.id.editTextFecha);
+        editTextCoste = findViewById(R.id.editTextCoste);
     }
 
     @Override
@@ -20,16 +37,21 @@ public class PantallaNuevaTarea extends AppCompatActivity {
         getMenuInflater().inflate(R.menu.action_bar_principal, menu);
         return true;
     }
+
     @Override
     public boolean onOptionsItemSelected(@NonNull MenuItem item) {
         ActionBarOpciones abOpciones = new ActionBarOpciones();
         int id = item.getItemId();
-        if (id==R.id.abContrasena) {
+        if (id == R.id.abContrasena) {
             abOpciones.abContrasena(this);
         }
-        if (id==R.id.abAcercaDe) {
+        if (id == R.id.abAcercaDe) {
             abOpciones.abAcercaDe(this);
         }
         return super.onOptionsItemSelected(item);
+    }
+
+    public void cancelar(View view) {
+        finish();
     }
 }
