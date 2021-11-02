9c538d2c8eb708665c350e729e50d812d713a5dd
asierhernandezga <asier.hernandezga@elorrieta-errekamari.com>
2021-11-02 13:24:50 +0100

    PantallaTarea metodos agregados

--- app/src/main/java/com/example/reto1/PantallaNuevaTarea.java
+++ app/src/main/java/com/example/reto1/PantallaNuevaTarea.java
@@ -3,8 +3,10 @@ package com.example.reto1;
 import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.ContentValues;
 import android.content.Context;
 import android.content.SharedPreferences;
+import android.database.sqlite.SQLiteDatabase;
 import android.os.Bundle;
 import android.view.Menu;
 import android.view.MenuItem;
@@ -13,6 +15,10 @@ import android.widget.ArrayAdapter;
 import android.widget.EditText;
 import android.widget.Spinner;
 import android.widget.Toast;
+
+import java.text.BreakIterator;
+import java.text.SimpleDateFormat;
+import java.util.Date;
 
 public class PantallaNuevaTarea extends AppCompatActivity {
 
@@ -53,5 +59,52 @@ public class PantallaNuevaTarea extends AppCompatActivity {
 
     public void cancelar(View view) {
         finish();
+    }
+
+    public void registrar(View view) {
+       String nombre = editTextNombre.getText().toString();
+       String descripcion = editTextDescripcion.getText().toString();
+       String fecha = editTextFecha.getText().toString();
+       String coste = editTextCoste.getText().toString();
+
+       Spinner spinnerPrioridadDatos = findViewById(R.id.spinnerPrioridad);
+       String prioridad = spinnerPrioridadDatos.getSelectedItem().toString();
+
+       Date fechaComprobada = comprobarFecha(fecha);
+
+       if(fechaComprobada != null){
+            alta(nombre, descripcion, coste, fechaComprobada, prioridad);
+       }else{
+           Toast.makeText(this,R.string.toastFechaIncorrecta,Toast.LENGTH_LONG).show();
+       }
+    }
+
+    public Date comprobarFecha(String fecha) {
+        Date fechaComprobada=null;
+
+        try {
+            SimpleDateFormat formato = new SimpleDateFormat("dd/MM/yyyy");
+            fechaComprobada = formato.parse(fecha);
+        }catch (Exception e){
+            fechaComprobada = null;
+        }
+        return fechaComprobada;
+    }
+
+    public void alta(String nombre, String descripcion, String coste, Date fecha, String prioridad) {
+        BDD admin = new BDD(this,
+                "administracion", null, 1);
+        SQLiteDatabase bd = admin.getWritableDatabase();
+
+        ContentValues registro = new ContentValues();
+        registro.put("nombre", nombre);
+        registro.put("descripcion", descripcion);
+        registro.put("fecha", String.valueOf(fecha));
+        registro.put("prioridad", prioridad);
+        registro.put("coste",coste);
+        bd.insert("tarea", null, registro);
+        bd.close();
+        Toast.makeText(this, "Se cargaron los datos de la tarea",
+                Toast.LENGTH_SHORT).show();
     }
 }
